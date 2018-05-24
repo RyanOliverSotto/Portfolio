5346e249737dfef9cb568bc5862ba5a748e5ddb0
ryanoliversotto <ryan.sotto@outlook.com>
2018-05-23 19:28:38 -0500

    2 bugs outstanding

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+//$(window).on("load"),function() {
+   // $(".loader .inner").fadeOut(500, function(){
+   //    $(".loader").fadeOut(750);
+  //  });
+        //$(".loader").fadeOut(500);
+//}
+
 $(document).ready(function(){
 
     $('#slides').superslides({
